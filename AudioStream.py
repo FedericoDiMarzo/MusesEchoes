c92ebcfcdebfa4fe044f83f8f1179da0a8e5fbd1
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2020-12-19 01:19:36 +0100

    AudioStream
    comments

--- AudioStream.py
+++ AudioStream.py
@@ -7,7 +7,14 @@ import time
 
 
 class AudioStream(object):
+    """
+    Stream of audio, implemented with pyaudio
+    """
+
     def __init__(self, rate):
+        """
+        :param rate: sample frequency
+        """
         # stream constants
         self.CHUNK = 2048
         self.FORMAT = pyaudio.paFloat32
@@ -25,24 +32,25 @@ class AudioStream(object):
             output=True,
             frames_per_buffer=self.CHUNK,
         )
+        print('stream opened')
 
     def read(self, number_of_blocks):
-        result = b''
+        """
+        reads from the stream
+
+        :param number_of_blocks: number of blocks of size chunk to read
+        :return: numpy float array containing the audio
+        """
+        result = b''  # empty binary string
 
         for i in range(number_of_blocks):
             result = result + self.stream.read(self.CHUNK)
 
         return np.fromstring(result, 'Float32')
 
-    def read_old(self, number_of_blocks):
-        result = ()
-
-        for i in range(number_of_blocks):
-            data = self.stream.read(self.CHUNK)
-            result = result + struct.unpack(str(2 * self.CHUNK) + 'B', data)
-
-        return np.array(result, dtype='b')[::2]
-
-    def exit_app(self):
-        print('stream closed')
+    def close(self):
+        """
+        closes the stream
+        """
         self.pyaudio.close(self.stream)
+        print('stream closed')
