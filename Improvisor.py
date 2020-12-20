e047716e26c7f36b04e0ebf5632083e942d5eda5
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2020-12-20 10:14:06 +0100

    Revert "Improvisor"
    This reverts commit 6481b2fec28058f1fbe4dd1995af5cc82c814561.

--- Improvisor.py
+++ Improvisor.py
@@ -1,18 +1,15 @@
 import numpy as np
 import mido
-from collections import Counter
 
 """
 list of notes in standard notation
 """
-note_std_list = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+note_list = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 """
 this data structure contains all the
 possible semitone sequences to construct
-the modes;
-
-each row defines a mode family
+the modes
 """
 semitone_sequences = [
     [2, 2, 1, 2, 2, 2, 1],  # TTSTTTS
@@ -20,85 +17,66 @@ semitone_sequences = [
 ]
 
 
-def midi_to_std(midi_note):
+def midi_to_note(midinote):
     """
     converts a midi note to standard note notation
 
-    :param midi_note: midi note number
+    :param midinote: midi note number
     :return: note in standard notation
     """
-    return note_std_list[midi_note % 12]
+    return note_list[midinote % 12]
 
 
-def get_root(notes_std):
+def get_root(notes):
     """
-    infers a root note from a list,
-    choosing the most played
 
-    :param notes_std: notes in standard notation
+    :param notes: notes in standard notation
     :return: single note in standard notation
     """
-    return Counter(notes_std).most_common(1)[0][0]
+    # TODO: extract root from most frequent note
+    pass
 
 
 def get_all_modes(root):
-    """
-    given a root note, generates all the possible modes
-
-    :param root: note in standard notation
-    :return: multi dimensional list of size [mode_families x 7 x 7] containing notes in standard notation
-    """
     modes_note_std = []
     for i in range(len(semitone_sequences)):  # iterating for different modes families
         modes_note_std.append([])
         current_sequence = np.array(semitone_sequences[i])
         for j in range(7):  # iterating for each mode in the family
             modes_note_std[i].append([root])
-            last_note = note_std_list.index(root)
+            last_note = note_list.index(root)
             for k in range(6):  # iterating for each note in the scale
                 last_note = (last_note + current_sequence[k]) % 12
-                modes_note_std[i][j].append(note_std_list[last_note])
+                modes_note_std[i][j].append(note_list[last_note])
             current_sequence = np.roll(current_sequence, -1)  # circular shift of the note sequence
     return modes_note_std
 
 
-def get_all_modes_for_each_note():
-    return {note: get_all_modes(note) for note in note_std_list}
-
-
-def harmonic_distances(modes, notes_std):
+def harmonic_distances(modes_note_std, notes_std):
     # TODO: implementation
     # step2: check the how many note are in the scale and how many not, and assign a distance to each
     pass
 
 
-all_modes = get_all_modes_for_each_note()
-
-
 class Improvisor:
     def __init__(self):
-        self.midiNoteList = []  # contains midi note, time information and probability
-        self.currentMode = {'root': 'C', 'mode_family_index': 0, 'mode_index': 0}
+        self.noteQueue = []
+        self.currentMode = {'root': 'C', 'semitone_sequence_index': 0, 'mode_index': 0}
 
-    def push_notes(self, note_messages):
-        """
-        pushes new notes messages inside midiNoteList
-
-        :param note_messages: contains the midi note, the absolute ticks for start and end
-        """
-        notes_and_probability = [{'midi': msg, 'probability': 0} for msg in note_messages]
-        self.midiNoteList.append(notes_and_probability)
+    def push_notes(self, new_notes):
+        notes_and_probability = [{'midinote': note, 'probability': 0} for note in new_notes]
+        self.noteQueue.append(notes_and_probability)
 
     def next_mode(self):
-        notes_std = [midi_to_std(note['midi']) for note in self.midiNoteList]
-        root = get_root(notes_std)
-        modes_distance = harmonic_distances(all_modes[root])
-        mode_family_index = modes_distance.index(min(modes_distance))
-        nearest_mode_family = modes_distance[mode_family_index]
-        mode_index = nearest_mode_family.index(min(nearest_mode_family))
+        notes = [note['midi'] for note in self.noteQueue]
+        root = get_root(notes)
+        modes_distance = harmonic_distances(root, notes)
+        semitone_sequences_index = modes_distance.index(min(modes_distance))
+        tmp = modes_distance[semitone_sequences_index]  # sequence with the lowest distance
+        mode_index = tmp.index(min(tmp))
 
         self.currentMode['root'] = root
-        self.currentMode['mode_family_index'] = mode_family_index
+        self.currentMode['semitone_sequence_index'] = semitone_sequences_index
         self.currentMode['mode_index'] = mode_index
 
 
--- tests/test_improvisor.py
+++ tests/test_improvisor.py
@@ -1,7 +1,7 @@
-from Improvisor import get_all_modes, note_std_list, read_midi
+from Improvisor import get_all_modes, note_list, read_midi
 
 if __name__ == '__main__':
-    for note in note_std_list:
+    for note in note_list:
         print("modes of " + note + ":")
         mode_list = get_all_modes(note)
         for i in range(len(mode_list)):
