b04d3b8fee56530eb45ec7f91e69128d22985245
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2020-12-20 16:29:55 +0100

    Update test_harmony2.py
    test update

--- tests/test_harmony2.py
+++ tests/test_harmony2.py
@@ -3,7 +3,7 @@ from harmony import get_all_modes, note_std_list, harmonic_affinities, get_root
 # test for harmonic_affinities
 if __name__ == '__main__':
     inputs = [
-        ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C', 'C'],
+        ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C', 'C', 'C'],
         ['C', 'D', 'D#', 'F', 'G', 'A', 'A#', 'C', 'C'],
         ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#', 'C', 'C'],
         ['D', 'D', 'E', 'F', 'G', 'A', 'A#', 'C', 'D'],
@@ -15,10 +15,11 @@ if __name__ == '__main__':
         modes = get_all_modes(root)
         affinities = harmonic_affinities(modes, x)
 
-        mode_signature_index = affinities.index(max(affinities))
-        tmp = affinities[mode_signature_index]
-        mode_index = tmp.index(max(tmp))
+        i = affinities.index(max(affinities))
+        tmp = affinities[i]
+        j = tmp.index(max(tmp))
 
         print('input:' + str(x))
         print('root note:' + root)
-        print('mode indices: ' + str(mode_signature_index) + " " + str(mode_index) + '\n\n------------')
+        print('mode indices: ' + str(i) + " " + str(j) + '\n\n------------')
+        print('affinity: ' + str(affinities[i][j]))
