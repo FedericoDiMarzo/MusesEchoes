22f0d846ff84d98be9885622ba9edca2db93fbeb
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-17 12:17:55 +0200

    Update __init__.py

---
+++ muses-echoes/__init__.py
@@ -0,0 +1,4 @@
+# TODO: change mode if N x notes are detected
+# TODO: change mode based on time not on buffer_len
+# TODO: separate melodic and rhythmic markov chains
+# TODO: check sequencer documentation
