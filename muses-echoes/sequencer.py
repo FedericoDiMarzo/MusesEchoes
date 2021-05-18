30f901ae7aa93320ca7845eb017f33919a1e41d8
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-05-19 00:30:28 +0200

    Update sequencer.py

--- muses-echoes/sequencer.py
+++ muses-echoes/sequencer.py
@@ -1,140 +1,49 @@
-import lib.braid as braid
-import numpy as np
-from harmony import mode_signatures, std_to_midi
+import melodically
+import threading
 import mido
-import sys
-from pythonosc.osc_server import BlockingOSCUDPServer, Dispatcher
-from threading import Thread
-import json
-
-ip = "127.0.0.1"
-port = 1337
-sequencer = None
-
-
-def get_scale_from_mode(mode_signature, mode_index):
-    rolled_mode_signature = list(np.roll(mode_signature, -mode_index))
-    braid_semitones = [0]
-    for index in range(6):
-        braid_semitones.append(braid_semitones[index] + rolled_mode_signature[index])
-
-    return braid.Scale(braid_semitones)
+import time
 
 
 class Sequencer:
-    def __init__(self, midi_out):
-        braid.midi_out = midi_out
-        self.complexity = 0  # 0 simple, 1 complex
-        self.tracks = {}
-        self.scores = {}
-        # braid.log_midi(True)
+    def __init__(self, midi_out_port, bpm=74):
+        self.midiOutPort = midi_out_port
+        self.bpm = bpm
+        self.durations = melodically.get_durations(self.bpm)
+        self.thread = threading.Thread(target=self.run)
+        self.playEvent = threading.Event()
+        self.lock = threading.Lock()
+        self.sequence = []
+        self.thread.start()
 
     def set_bpm(self, bpm):
-        print('bpm changed: {}'.format(bpm))
-        braid.tempo(bpm)
+        self.durations = melodically.get_durations(bpm)
+        self.bpm = bpm
 
-    def calculate_complexity(self, notes_per_seconds):
-        max_notes_per_seconds = 10  # TODO: fine tune the value
-        self.complexity = min(max_notes_per_seconds, notes_per_seconds) / max_notes_per_seconds
-        print('sequencer complexity: {}'.format(self.complexity))
+    def play_sequence(self, sequence):
+        # critical section
+        with self.lock:
+            self.sequence = sequence
+        # end of critical section
 
-    def change_mode(self, mode):
-        mode_sig = mode_signatures[mode['mode_signature_index']]
-        scale = get_scale_from_mode(mode_sig, mode['mode_index'])
-        for t in self.tracks.values():
-            t.chord = std_to_midi(mode['root']), scale
+        self.playEvent.set()
 
-    def add_track(self, midi_channel):
-        self.tracks[midi_channel] = braid.Thread(midi_channel)
+    def run(self):
+        while True:
+            self.playEvent.wait()
+            self.playEvent.clear()
 
-    def set_score(self, track_number, score):
-        self.scores[track_number] = score
+            with mido.open_output(self.midiOutPort) as outport:
+                # critical section
+                with self.lock:
+                    for step in self.sequence:
+                        note_on = mido.Message('note_on', note=step['note'])
+                        note_off = mido.Message('note_off', note=step['note'])
 
-    def set_pattern(self, track_number):
-        simple_pattern = self.scores[track_number]['simple']
-        complex_pattern = self.scores[track_number]['complex']
-        blended_pattern = braid.blend(simple_pattern, complex_pattern, self.complexity)
-        print(blended_pattern)  # TODO: remove print
-        self.tracks[track_number].pattern = blended_pattern
+                        if 'r' not in step['duration']:
+                            outport.send(note_on)
 
-    def play(self):
-        braid.play()
+                        time.sleep(self.durations[step['duration'].replace('r', '')])
 
-
-def setup_tracks(seq):
-    seq.add_track(1)
-    seq.add_track(2)
-    seq.add_track(3)
-
-    score1 = {
-        'simple': (1, 0, 0, braid.Z),
-        'complex': (1, 0, 0, braid.Z)
-    }
-
-    score2 = {
-        'simple': ([3, 5], 0, [5, 3], 0),
-        'complex': ([5, 7], [5, [5, 3]], [5, 1], [1, [5, 1]])
-    }
-
-    score3 = {
-        'simple': (0, 5, 1, 0),
-        'complex': ([3, [5, 3]], [[3, 7], 3], [[5, 5, 5], 3], [5, [7, 5, 3, 5], [7, 5]])
-    }
-
-    seq.set_score(1, score1)
-    seq.set_score(2, score2)
-    seq.set_score(3, score3)
-    seq.set_pattern(1)
-    seq.set_pattern(2)
-    seq.set_pattern(3)
-
-
-def osc_handler(address, *args):
-    # TODO: check for possible concurrency issues
-    # TODO: separate OSC handlers
-    mode = json.loads(args[0])
-    notes_per_second = float(args[1])
-    print('OSC address: {}'.format(address))
-    print('current mode',
-          'root: {}'.format(mode['root']),
-          'mode_signature_index: {}'.format(mode['mode_signature_index']),
-          'mode_index: {}'.format(mode['mode_index']),
-          'notes_per_second: {}'.format(notes_per_second),
-          '',
-          sep='\n')
-    sequencer.change_mode(mode)
-    sequencer.calculate_complexity(notes_per_second)
-    for i in range(len(sequencer.tracks)):
-        sequencer.set_pattern(i + 1)
-
-
-def osc_server_setup():
-    print('OSC server running')
-    dispatcher = Dispatcher()
-    dispatcher.map("/sequencer/*", osc_handler)
-    server = BlockingOSCUDPServer((ip, port), dispatcher)
-    server.serve_forever()  # Blocks forever
-
-
-if __name__ == '__main__':
-    midi_output_names = mido.get_output_names()
-
-    if len(sys.argv) <= 1:
-        print('usage: python sequencer.py midi_port\n',
-              'midi ports: {}'.format(midi_output_names),
-              sep='\n')
-        exit(-1)
-
-    # OSC
-    osc_thread = Thread(target=osc_server_setup)
-    osc_thread.start()
-
-    # setting up midi out
-    midi_out = int(sys.argv[1])
-
-    # setting up the sequencer
-    sequencer = Sequencer(midi_out)
-    setup_tracks(sequencer)
-    sequencer.set_bpm(80)
-    sequencer.change_mode({'root': 'C', 'mode_signature_index': 0, 'mode_index': 0})
-    sequencer.play()
+                        if 'r' not in step['duration']:
+                            outport.send(note_off)
+                # end of critical section
