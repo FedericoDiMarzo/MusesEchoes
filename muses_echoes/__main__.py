0a1fd6a651389f49d92c332a2057d4bce4c219c3
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-06-01 10:56:37 +0200

    Update __main__.py
    rhythm out

--- muses_echoes/__main__.py
+++ muses_echoes/__main__.py
@@ -10,12 +10,13 @@ settings of the application.
 _osc_ip = "127.0.0.1"
 _osc_port = 1337
 _midi_buffer_size = 3
-#_midi_mapping = [1, 2, 3, 3, 1, 2, 3]  # 1: I-V / 2: II-VI / 3: III-IV-VII
+# _midi_mapping = [1, 2, 3, 3, 1, 2, 3]  # 1: I-V / 2: II-VI / 3: III-IV-VII
 _midi_mapping = [1, 1, 1, 1, 1, 1, 1]  # TODO: Test
 _bpm = 74
 _measures_for_scale_change = 4
 _melody_octave_range = (4, 5)
 _chord_octave_range = (3, 4)
+_rhythm_midi_note = 24  # C1
 _markov_chains_order = 3
 _markov_chains_inertia = 0.81
 
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     midi_output_names = mido.get_output_names()
 
     if len(sys.argv) <= 3:
-        print('usage: python -m muses_echoes [in] [melodies] [chords]',
+        print('usage: python -m muses_echoes [in] [melodies] [chords] [rhythm]',
               '__________\n',
               'midi in ports: {}'.format(midi_input_names),
               'midi out ports: {}'.format(midi_output_names),
@@ -35,6 +36,7 @@ if __name__ == '__main__':
               '[in]: midi in port index used to receive notes',
               '[melodies]: midi out port index used to send notes for the melody',
               '[chords]: midi out port index used to send notes for the chords',
+              '[rhythm]: midi out port index used to send notes for the rhythm',
               '',
               sep='\n')
         exit(-1)
@@ -42,9 +44,11 @@ if __name__ == '__main__':
     midi_in_index = int(sys.argv[1])
     midi_sequence_out_index = int(sys.argv[2])
     midi_chord_out_index = int(sys.argv[3])
+    midi_rhythm_out_index = int(sys.argv[4])
     print('selected midi input: {}'.format(midi_input_names[midi_in_index]),
           'selected midi sequence output: {}'.format(midi_output_names[midi_sequence_out_index]),
           'selected midi chord output: {}'.format(midi_output_names[midi_chord_out_index]),
+          'selected midi rhythm output: {}'.format(midi_output_names[midi_rhythm_out_index]),
           '',
           sep='\n')
     # =========================================
@@ -55,6 +59,7 @@ if __name__ == '__main__':
         midi_in_port=midi_input_names[midi_in_index],
         midi_sequence_out_port=midi_output_names[midi_sequence_out_index],
         midi_chord_out_port=midi_output_names[midi_chord_out_index],
+        midi_rhythm_out_port=midi_output_names[midi_rhythm_out_index],
         midi_mapping=_midi_mapping,
         osc_ip=_osc_ip,
         osc_port=_osc_port,
@@ -63,6 +68,7 @@ if __name__ == '__main__':
         measures_for_scale_change=_measures_for_scale_change,
         melody_octave_range=_melody_octave_range,
         chord_octave_range=_chord_octave_range,
+        rhythm_midi_note=_rhythm_midi_note,
         markov_chains_order=_markov_chains_order,
         markov_chains_inertia=_markov_chains_inertia
     )
